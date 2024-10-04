2986da42b5d1783c566becee3d647dde0ebd160a
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-10-04 10:41:06 +0800

    add MyServicesRoute

---
+++ src/routes/MyServicesRoute.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import multer from "multer";
+import { jwtCheck, jwtParse } from "../middlewares/auth";
+import MyServicesController from "../controllers/MyServicesController";
+
+const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, //5mb
+  },
+});
+
+// api/my/services
+
+router.get("/", jwtCheck, jwtParse, MyServicesController.getMyServiceProvider);
+
+router.post(
+  "/",
+  upload.single("imageFile"),
+  jwtCheck,
+  jwtParse,
+  MyServicesController.createMyServiceProvider
+);
+
+router.put(
+  "/",
+  jwtCheck,
+  jwtParse,
+  MyServicesController.updateMyServiceProvider
+);
